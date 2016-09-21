3f7ea2b7a81fceb1d6de85494c6bfc659e40d9ed
Nick DiQuattro <ndiquattro@gmail.com>
2016-09-21 12:56:03 -0700

    implemented cluster algorithim

--- app/logic/nlps.py
+++ app/logic/nlps.py
@@ -1,12 +1,14 @@
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, HashingVectorizer
+from sklearn.cluster import KMeans
 import numpy as np
 import pandas as pd
 from nltk.corpus import stopwords
 from nltk.tokenize import casual
 
 # Construct stopwords
-en_stop = set(stopwords.words("english")).union(['http', 'https', 'rt', 'co',
-                                                 'like'])
+custom_stop = ['http', 'https', 'co', 'rt', 'like', 'official', 'twitter',
+               'account', 'tweets']
+en_stop = set(stopwords.words("english")).union(custom_stop)
 
 
 # Bag of words
@@ -35,3 +37,55 @@ def word_bagger(tlist):
 
     # Return a Pandas Dataframe
     return word_counts
+
+
+# Kmeans based approach
+def cluster_terms(docs):
+    # Set up vectorizor
+    vectorizer = HashingVectorizer(stop_words=en_stop, norm='l2',
+                                   ngram_range=(1, 2))
+
+    # Vectorize
+    counts = vectorizer.fit_transform(docs)
+
+    # Fit Kmeans model
+    km = KMeans(n_clusters=5)
+    idx = km.fit_predict(counts)
+
+    # Label descriptions
+    clust_docs = pd.DataFrame(zip(docs, idx))
+    clust_docs.columns = ['text', 'cluster']
+    clust_docs = clust_docs.groupby(['cluster'])
+
+    # Term Finder
+    def term_finder(docs, counter):
+        # Count up terms for this cluster
+        counts = counter.fit_transform(docs).toarray()
+
+        # Combine with vocab into a dataframe
+        vocab = counter.get_feature_names()
+        dist = np.sum(counts, axis=0)
+
+        word_counts = []
+        for tag, count in zip(vocab, dist):
+            word_counts += [{'count': count, 'word': tag}]
+
+        word_counts = pd.DataFrame.from_dict(word_counts)
+        word_counts = word_counts.sort_values('count', ascending=False)
+
+        # Get top unigrams and bigrams
+        unis = word_counts[
+            word_counts.word.apply(lambda x: len(x.split()) == 1)].head(2)
+        bis = word_counts[
+            word_counts.word.apply(lambda x: len(x.split()) == 2)].head(2)
+
+        # Combine
+        topdf = pd.concat([unis, bis])
+
+        return topdf
+
+    # Get Term Frequency for each cluseter
+    counter = CountVectorizer(ngram_range=(1, 2), stop_words=en_stop)
+    term_counts = clust_docs['text'].apply(term_finder, counter)
+
+    return term_counts
